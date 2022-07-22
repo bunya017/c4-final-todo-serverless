8ec9a58f16e46b4ff3f26e3d274e9d2d64930693
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 15:16:33 +0100

    chore(http): Add `createTodo` handler

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,15 +4,20 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils';
-import { createTodo } from '../../businessLogic/todos'
+import { createTodo } from '../../helpers/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
     // TODO: Implement creating a new TODO item
+    const userId = getUserId(event)
+    const todoItem = await createTodo(userId, newTodo)
 
-    return undefined
-)
+    return {
+      statusCode: 201,
+      body: JSON.stringify({ item: todoItem })
+    }
+})
 
 handler.use(
   cors({
