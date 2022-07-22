0872999822e3d26fa1ad5862a7496a1453bdfb08
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 15:11:19 +0100

    refactor(attachmentUtils): Add `getUploadUrl()`

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -4,3 +4,16 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
+const s3 = new XAWS.S3({ signatureVersion: 'v4' })
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+
+export async function getUploadUrl(attachmentId: string): Promise<string> {
+  const uploadUrl = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: attachmentId,
+    Expires: parseInt(urlExpiration)
+  })
+
+  return uploadUrl
+}
