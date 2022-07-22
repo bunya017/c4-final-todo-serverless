4c3308c8d373acb2009df1dd3e739487bb0edbb3
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 21:47:00 +0100

    fix(todoAccess): Fix AWSXRay import

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -1,11 +1,11 @@
 import 'source-map-support/register'
 import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
 import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 import { createLogger } from '../utils/logger'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate';
 
+const AWSXRay = require('aws-xray-sdk')
 const XAWS = AWSXRay.captureAWS(AWS)
 
 const logger = createLogger('TodosAccess')
