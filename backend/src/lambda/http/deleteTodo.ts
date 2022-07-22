4643ab22ccc7f06cefc707202762633cc2cf96b6
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 15:16:52 +0100

    chore(http): Add `deleteTodo` handler

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,17 +4,25 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../helpers/todos'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Remove a TODO item by id
-    
-    return undefined
-  }
-)
+    try {
+      const userId = getUserId(event)
+      const item = await deleteTodo(todoId, userId)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ item })
+      }
+
+    } catch (error) {
+      throw new Error(error)
+    }
+})
 
 handler
   .use(httpErrorHandler())
