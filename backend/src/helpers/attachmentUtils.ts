9895ce259b5411111cd7d3404571dd8049057b7f
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 15:11:42 +0100

    refactor(attachmentUtils): Add `getAttachmentUrl()`

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -8,6 +8,10 @@ const s3 = new XAWS.S3({ signatureVersion: 'v4' })
 const bucketName = process.env.ATTACHMENT_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
+export async function getAttachmentUrl(attachmentId: string): Promise<string> {
+   return `https://${bucketName}.s3.amazonaws.com/${attachmentId}`
+}
+
 export async function getUploadUrl(attachmentId: string): Promise<string> {
   const uploadUrl = s3.getSignedUrl('putObject', {
     Bucket: bucketName,
