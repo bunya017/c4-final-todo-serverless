db371a5400447f9d9d71f4ba66d73e54e39762fb
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 15:17:34 +0100

    chore(http): Add `getTodos` handler

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,16 +4,24 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getTodosForUser as getTodosForUser } from '../../helpers/todos'
 import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
-
-    return undefined
+    try {
+      const userId = getUserId(event)
+      const todos = await getTodosForUser(userId);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ items: todos })
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+})
 
 handler.use(
   cors({
