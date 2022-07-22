25c8874196adbfcec2a053bb294ed1cfedc152e4
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-22 15:45:11 +0100

    fix(http/updateTodo): Fix argument order

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -14,7 +14,7 @@ export const handler = middy(
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
     const userId = getUserId(event)
-    await updateTodo(updatedTodo, todoId, userId)
+    await updateTodo(userId, todoId, updatedTodo)
     return {
       statusCode: 201,
       body: JSON.stringify({})
