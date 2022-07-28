baa35a63a05a631620cd23975fdca3b3a76442a5
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-28 22:18:24 +0100

    refactor(updateTodo): Add logger

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -4,19 +4,29 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 import { updateTodo } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+
+
+const logger = createLogger('updateTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  const userId = getUserId(event)
-  await updateTodo(userId, todoId, updatedTodo)
-  return {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify({})
+  try {
+    const userId = getUserId(event)
+    await updateTodo(userId, todoId, updatedTodo)
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({})
+    }
+    logger.info('Updated successfully')
+  } catch (error) {
+    logger.error(error)
+    throw new Error(error);
   }
 }
