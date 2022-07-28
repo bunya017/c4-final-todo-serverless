4c38066e1c9607e8dc8578203cf300454a507826
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-07-28 23:52:36 +0100

    refactor(config): Add `apiId`

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '0p1vtxcg16'
+const apiId = '82w681pgx5'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
